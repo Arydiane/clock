291bb06bc7361d65892698db3e795d80c8a912c9
Arydiane <arydianejardim@gmail.com>
2022-11-16 18:46:19 -0300

    feature - adiciona informações sobre data

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -6,9 +6,21 @@ import { MdDarkMode, MdWbSunny } from "react-icons/md";
 
 export default function Clock() {
 
+    const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    const daysWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
     let now = new Date()
+
     const [time, setTime] = useState(() => { return now.toLocaleTimeString() })
-    const [location, setLocation] = useState(() => { return { city: "procurando localização", countryCode: "" } })
+    const [location, setLocation] = useState(() => { return { city: "procurando localização", countryCode: "", timezone: "local desconhecido" } })
+    const [currentDate, setcurrentDate] = useState(() => {
+        return {
+            day: now.getDate(),
+            month: now.getMonth(),
+            year: now.getFullYear(),
+            dayWeek: now.getDay()
+        }
+    })
 
     useEffect(() => {
         const api = async () => {
@@ -17,12 +29,14 @@ export default function Clock() {
             if (data.status === 'success') {
                 setLocation({
                     city: data.city,
-                    countryCode: data.countryCode
+                    countryCode: data.countryCode,
+                    timezone: data.timezone
                 })
             } else {
                 setLocation({
                     city: "local desconhecido",
-                    countryCode: ""
+                    countryCode: "",
+                    timezone: "local desconhecido"
                 })
             }
         }
@@ -51,19 +65,33 @@ export default function Clock() {
     setInterval(updateTime, 1000)
 
     return (
-        <section
-            className={styles.clock}
-            style={{ backgroundImage: time.slice(0, 2) < 18 ? `url(${backgroundDay})` : `url(${backgroundNight})` }}
-        >
-            <div className={styles.clock__container}>
-                <p className={styles.clock__greeting}>
-                    {greetingMessage(time.slice(0, 2))}
-                </p>
-                <p className={styles.clock__time}>{time}</p>
-                <p className={styles.clock__location}> 
-                    em {location.city}, {location.countryCode}
-                </p>
-            </div>
-        </section>
+        <>
+            <section
+                className={styles.clock}
+                style={{ backgroundImage: time.slice(0, 2) < 18 ? `url(${backgroundDay})` : `url(${backgroundNight})` }}
+            >
+                <div className={styles.clock__container}>
+                    <p className={styles.clock__greeting}>
+                        {greetingMessage(time.slice(0, 2))}
+                    </p>
+                    <p className={styles.clock__time}>{time}</p>
+                    <p className={styles.clock__location}>
+                        em {location.city}, {location.countryCode}
+                    </p>
+                </div>
+                <article
+                    className={styles.information}
+                    style={{ backgroundColor: time.slice(0, 2) < 18 ? "$background-day" : "$background-night" }}
+                >
+                    <div className={styles.information__container}>
+                        <p className={styles.information__text}>Fuso horário: <span>{location.timezone}</span></p>
+                        <p className={styles.information__text}>Dia da semana: <span>{daysWeek[currentDate.dayWeek]}</span></p>
+                        <p className={styles.information__text}>Data: <span>{currentDate.day} de {months[currentDate.month]} de {currentDate.year}</span></p>
+                    </div>
+
+                </article>
+            </section>
+
+        </>
     )
 }
