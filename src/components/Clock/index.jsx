ef0cdac413845d5f5781c2a3c8efadcd054df6f4
Arydiane <arydianejardim@gmail.com>
2022-11-16 17:24:44 -0300

    feature - exibe horário atualizado

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Clock.module.scss"
+import backgroundDay from "assets/images/day-medium.jpg"
+import backgroundNight from "assets/images/night-medium.jpg"
+import { MdDarkMode, MdWbSunny } from "react-icons/md";
 
-export default function Clock(){
+export default function Clock() {
 
     let now = new Date()
+    const [time, setTime] = useState(() => { return now.toLocaleTimeString() })
+    const [location, setLocation] = useState(() => { return { city: "procurando localização", countryCode: "" } })
 
-    const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-    const daysWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 
-    const day = now.getDate()
-    const month = now.getMonth()
-    const year = now.getFullYear()
-    const dayWeek = now.getDay()
-    const hour = now.getHours()
-    const min = now.getMinutes()
-    
+
+    function updateTime() {
+        now = new Date()
+        setTime(now.toLocaleTimeString())
+    }
+
+    function greetingMessage(hour) {
+        let message = ''
+
+        if (hour > 0 && hour < 12) {
+            message = <> <MdWbSunny /> Bom dia! </>
+        } else if (hour < 18) {
+            message = <> <MdWbSunny /> Boa tarde! </>
+        } else {
+            message = <> <MdDarkMode /> Boa noite! </>
+        }
+
+        return message
+    }
+
+    setInterval(updateTime, 1000)
+
     return (
-        <section className={styles.clock__container}>
-            <p className={styles.clock__greeting}>Mensagem: Boa tarde!</p>
-            <p className={styles.clock__time}>{hour}:{min}</p>
-            <p className={styles.clock__date}>{daysWeek[dayWeek]}, {day} de {months[month]} de {year}</p>
-            <p className={styles.clock__location}>Cidade, Pais</p>
-
+        <section
+            className={styles.clock}
+            style={{ backgroundImage: time.slice(0, 2) < 18 ? `url(${backgroundDay})` : `url(${backgroundNight})` }}
+        >
+            <div className={styles.clock__container}>
+                <p className={styles.clock__greeting}>
+                    {greetingMessage(time.slice(0, 2))}
+                </p>
+                <p className={styles.clock__time}>{time}</p>
+                <p className={styles.clock__location}> em {location.city}, {location.countryCode}</p>
+            </div>
         </section>
     )
 }
